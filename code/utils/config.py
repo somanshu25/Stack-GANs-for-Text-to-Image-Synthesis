3ff431381f37b23f816d7eeda9af34d04ef1c4a9
Somanshu Agarwal <somanshu@huntsman-ve703-10486.apn.wlan.private.upenn.edu>
2019-11-30 19:01:43 -0500

    Changes in config file

--- code/utils/config.py
+++ code/utils/config.py
@@ -1,1 +1,31 @@
 import numpy as np
+import os.path as osp
+from easydict import EasyDict as edict
+
+__C = edict()
+cfg = __C
+
+# Config variables for General 
+
+__C.TextDim = 1024
+__C.LatentDim = 100
+__C.ImageSizeStageI = 64
+__C.ImageSizeStageII = 256 
+
+# Config variables for Training GANS
+
+__C.BatchSize = 64
+__C.NumEpochs = 600
+__C.LrDecayEpoch = 600
+__C.DiscLR = 0.0002
+__C.GenLR = 0.0002
+__C.KLCoeff = 2
+
+# Config parameters for mode options for GANS
+
+__C.ConditionDim = 128
+__C.DiscInputDim = 64
+__C.GenInputDim = 128
+
+
+
