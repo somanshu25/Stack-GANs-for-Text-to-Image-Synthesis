2f620d2ac5e9701e7c4dbf4c36d703f088b0f90d
hemanthkothapalli <44911234+hemanthkothapalli@users.noreply.github.com>
2019-12-03 16:28:42 -0500

    Update stage-II_code.py

--- code/stage-II/stage-II_code.py
+++ code/stage-II/stage-II_code.py
@@ -36,21 +36,21 @@ class GenStageII(nn.Module):
     self.network()
 
 
-    def network(self):
-      GID = self.gDim
-      self.downSamplingAtomic = downSamplingAtomic()
-      self.ConditioningAugment = ConditioningAugment()
-      self.encoder = nn.Sequential(
-          nn.Conv2d(3,GID,3,stride = 1, padding =1 , bias = False),
-          nn.ReLU(True))
-      self.downSample1 = self.downSamplingAtomic(GID, GID*2)
-      self.downSample2 = self.downSamplingAtomic(GID*2, GID*4)
-      
-      self.HighRes = nn.Sequential(
-          nn.Conv2d(self.cDim+GID*4, GID*4, 3, stride = 1, padding =1, bias = False),
-          nn.BatchNorm2d(GID*4)
-          nn.ReLU(True)
-      )
+  def network(self):
+    GID = self.gDim
+    self.downSamplingAtomic = downSamplingAtomic()
+    self.ConditioningAugment = ConditioningAugment()
+    self.encoder = nn.Sequential(
+        nn.Conv2d(3,GID,3,stride = 1, padding =1 , bias = False),
+        nn.ReLU(True))
+    self.downSample1 = self.downSamplingAtomic(GID, GID*2)
+    self.downSample2 = self.downSamplingAtomic(GID*2, GID*4)
+    
+    self.HighRes = nn.Sequential(
+        nn.Conv2d(self.cDim+GID*4, GID*4, 3, stride = 1, padding =1, bias = False),
+        nn.BatchNorm2d(GID*4)
+        nn.ReLU(True)
+    )
 
       
     # def _make_layer(self, block, channel_num):
@@ -59,39 +59,39 @@ class GenStageII(nn.Module):
     #         layers.append(block(channel_num))
     #     return nn.Sequential(*layers)
 
-      self.residual = self._make_layer(ResBlock, GID * 4)
-      
-      self.upSample1 = upSamplingAtomic(GID * 4, GID * 2)
-      self.upSample2 = upSamplingAtomic(GID * 2, GID)
-      self.upSample3 = upSamplingAtomic(GID, GID // 2)
-      self.upSample4 = upSamplingAtomic(GID // 2, GID // 4)
-     
-      self.img = nn.Sequential(
-          nn.Conv2d(GID // 4, 3, 3, stride = 1, padding =1, bias = False),
-          nn.Tanh())
+    self.residual = self._make_layer(ResBlock, GID * 4)
+    
+    self.upSample1 = upSamplingAtomic(GID * 4, GID * 2)
+    self.upSample2 = upSamplingAtomic(GID * 2, GID)
+    self.upSample3 = upSamplingAtomic(GID, GID // 2)
+    self.upSample4 = upSamplingAtomic(GID // 2, GID // 4)
+    
+    self.img = nn.Sequential(
+        nn.Conv2d(GID // 4, 3, 3, stride = 1, padding =1, bias = False),
+        nn.Tanh())
 
-      def forward(self, textEmbedding, noise):
-        a, imgStageI, c, d = self.GenStageI(textEmbedding, noise)
-        imgStageI = imgStageI.detach()
-        encodedImg = self.encoder(imgStageI)
-        tempencodedImg = self.downSample1(encodedImg)
-        FinEncodedImg = self.downSample2(tempencodedImg)
+  def forward(self, textEmbedding, noise):
+    a, imgStageI, c, d = self.GenStageI(textEmbedding, noise)
+    imgStageI = imgStageI.detach()
+    encodedImg = self.encoder(imgStageI)
+    tempencodedImg = self.downSample1(encodedImg)
+    FinEncodedImg = self.downSample2(tempencodedImg)
 
-        temp, mu, var = self.ConditioningAugment(textEmbedding)
-        temp = temp.view(-1, self.cDim, 1, 1)
-        temp = temp.repeat(1, 1, 16, 16)
-        temp1 = torch.cat([FinEncodedImg, temp], 1)
-        temp2 = self.HighRes(temp1)
-        temp2 = self.residual(temp2)
+    temp, mu, var = self.ConditioningAugment(textEmbedding)
+    temp = temp.view(-1, self.cDim, 1, 1)
+    temp = temp.repeat(1, 1, 16, 16)
+    temp1 = torch.cat([FinEncodedImg, temp], 1)
+    temp2 = self.HighRes(temp1)
+    temp2 = self.residual(temp2)
 
-        temp2 = self.upSample1(temp2)
-        temp2 = self.upSample2(temp2)
-        temp2 = self.upSample3(temp2)
-        temp2 = self.upSample4(temp2)
+    temp2 = self.upSample1(temp2)
+    temp2 = self.upSample2(temp2)
+    temp2 = self.upSample3(temp2)
+    temp2 = self.upSample4(temp2)
 
-        fakeImg = self.img(temp2)
+    fakeImg = self.img(temp2)
 
-        return imgStageI, fakeImg, mu, var
+    return imgStageI, fakeImg, mu, var
 
 
 
@@ -100,6 +100,7 @@ class GenStageII(nn.Module):
 
       
 class DiscStageII(nn.Module):
+  
   def __init__(self):
     super(DiscStageII, self).__init__()
     self.dDim = cfg.DiscInputDim
@@ -130,12 +131,14 @@ class DiscStageII(nn.Module):
     self.condLogits = LogitsForDiscriminator(DID, CD, bcondition = True)
     self.uncondLogits = LogitsForDiscriminator(DID, CD, bcondition = False)
 
-    def forward(self, img):
-      imgEmbedding = self.ImgEncode(img)
-      downSample1 = self.down1(imgEmbedding)
-      downSample2 = self.down2(downSample1)
-      downSample3 = self.down3(downSample2)
-      downSample4 = self.down4(downSample3)
-      downSample5 = self.down5(downSample4)
-      finalOutput = self.tempEncode(downSample5)
-      return finalOutput 
+  def forward(self, img):
+
+    imgEmbedding = self.ImgEncode(img)
+    downSample1 = self.down1(imgEmbedding)
+    downSample2 = self.down2(downSample1)
+    downSample3 = self.down3(downSample2)
+    downSample4 = self.down4(downSample3)
+    downSample5 = self.down5(downSample4)
+    finalOutput = self.tempEncode(downSample5)
+    return finalOutput 
+        
