0dab749beae54ef2cf632b04019801374e23c6fb
siddhivelankar <43520241+siddhivelankar@users.noreply.github.com>
2019-12-03 20:37:39 -0500

    Update common.py

--- code/utils/common.py
+++ code/utils/common.py
@@ -107,7 +107,36 @@ class LogitsForDiscriminator(nn.Module):
         return output.view(-1)
 
 
+def computeGeneratorLoss(netDisc,fakeImages,realLabels,conditions,gpus):
+    criterion = nn.BCELoss()
+    cond = conditions.detach()
 
+    ## fake logits and error
+    _,fakeLogits = nn.parallel.data.parallel(netDisc,(fakeImages,cond),gpus)
+    errorDiscFake = criterion(fakeLogits,realLabels)
+
+    return errorDiscFake
+
+def computeDiscriminatorLoss(netDisc,fakeImages,realImages,fakeLabels,realLabels,conditions,gpus):
+    criterion = nn.BCELoss()
+    cond = conditions.detach()
+    fakeImages = fakeImages.detach()
+
+    ## fake pairs
+    fakeEmbedding = nn.parallel.data.parallel(netDisc,(fakeImages),gpus)
+    fakeLogits = nn.parallel.data.parallel(netDisc.getConditionalLogits,(fakeEmbedding,cond),gpus)
+    errDiscFake = criterion(fakeLogits,realLabels)
+
+    ## real paris
+    realEmbedding = nn.parallel.data.parallel(netDisc,(realImages),gpus)
+    realLogits = nn.parallel.data.parallel(netDisc.getConditionalLogits,(realEmbedding,cond),gpus)
+    errDiscReal = criterion(realLogits,realLabels)
+
+    ## wrong paris
+    wrongLogits = nn.parallel.data.parallel(netDisc.getConditionalLogits,(realEmbedding[:realImages.shape[0]-1],cond[1:0]),gpus)
+    errDiscWrong = criterion(wrongLogits,fakeLabels[1:0])
+
+    return errDiscReal + 0.5*errDiscFake + 0.5*errDiscWrong, errDiscReal, errDiscWrong, errDiscFake
 
 
     
