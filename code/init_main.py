3b1e3e2254592fdff2fa819708babea8001f655e
Somanshu Agarwal <somanshu@huntsman-ve703-10843.apn.wlan.private.upenn.edu>
2019-12-03 20:43:24 -0500

    Changes

--- code/init_main.py
+++ code/init_main.py
@@ -1,1 +1,20 @@
 import numpy as np
+import torch.backends.cudnn as cudnn
+import torch
+import torchvision.transforms as transforms
+
+import argparse
+import os
+import random
+import sys
+import pprint
+import datetime
+import dateutil
+import dateutil.tz
+
+from utils.datasets import TextDataset
+from utils.config import cfg
+from utils.common import make_dir,STAGE1_GAN
+
+if __name__ == "__main__":
+    now = 
